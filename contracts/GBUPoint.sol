ff3ddf4df118e45cb258cc7e9857841c25e3d90f
heemin1008 <minco777@naver.com>
2023-05-31 14:16:52 +0900

    깃허브에 올리기

--- contracts/GBUPoint.sol
+++ contracts/GBUPoint.sol
@@ -14,4 +14,22 @@ contract GBUPoint {
      function Get_Point_Symbol() public view returns (string memory){
         return pointSymbol;
     }
+  /* 사용자가 보상을 요청 --> 필요한 정보 입력 (주소)  --> 승인/거절 (관리자 입장) --> 전송하기*/
+    event RequestSubmitted(address requester);
+    event RequestApproved(address requester, uint amount);
+    event RequestRejected(address requester, string reason);
+
+    enum RequestStatus { None, Requested, RequestConfirmed , RequestApproved, RequestRejected }
+    RequestStatus public requestStatus; // 요청 상태
+    address public requester; // 요청자의 주소
+ 
+    // 사용자는 포인트 요청하기 페이지에서 사용자의 지갑 주소를 입력하면 된다 --> 요청 완료  
+    function pointRequest(address _addr) public {
+        require(_addr != address(0), "Invalid address"); // 주소 유효성 검사
+
+        requester = _addr;
+        requestStatus = RequestStatus.Requested;
+    }
+
+
 }

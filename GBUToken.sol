c370f369b6eafe2af34a4755947b0827af1830e2
heemin1008 <125268228+heemin1008@users.noreply.github.com>
2023-06-07 15:25:15 +0900

    Update GBUToken.sol

--- GBUToken.sol
+++ GBUToken.sol
@@ -8,9 +8,7 @@ contract GBUToken is ERC20 {
     string private Symbol = "Token";
     uint private TokenTotalSupply = 0;
     address public admin;
-    //mapping (address => uint) private Tokenbalance;
     mapping (address => uint) private token_balances; 
-
 
     modifier onlyAdmin {
         require(msg.sender == admin, "Only admin can call this function.");
@@ -31,22 +29,41 @@ contract GBUToken is ERC20 {
         emit Transfer(address(0), _addr, _amount);
     }
 
-    //관리자 GBU Token 잔액 조회 기능 
-    function balanceOfAdmin() public view returns (uint256) {
+    // 관리자의 토큰 잔액 조회
+    function Token_Balance_Of_Admin() public view returns (uint256) {
         return balanceOf(admin);
     }
 
-    //관리자의 토큰 잔액 조회
-    function Token_Balance_Of_Admin() public view returns (uint256) {
-    return balanceOf(admin);
+    // 사용자의 토큰 잔액 조회
+    function Token_Balance_Of_User() public view returns (uint256) {
+        return token_balances[msg.sender];
     }
 
+    // 토큰 전송하기
     function transferToken(address to, uint256 amount) public returns (bool) {
-        require(amount > 0, "Invalid amount");
-        address owner = _msgSender();
-        _transfer(owner, to, amount);
+        require(amount > 0, "Invalid amount"); // amount가 0보다 커야함 
+
+        // 토큰 컨트랙트의 transfer 함수 호출
+        bool success = transfer(to, amount);
+
+        // Transfer 이벤트 발생
+        emit Transfer(msg.sender, to, amount);
+
+        // 관리자의 토큰 잔액과 사용자의 토큰 잔액 갱신 (해결) 
         token_balances[admin] -= amount;
         token_balances[to] += amount;
+
+        return success;  
+    }
+
+
+     //GBU Token 전송 기능
+      function TestTransfer(address _to, uint256 _amount) public virtual  returns (bool) {
+        _transfer(admin, _to, _amount);
+        token_balances[admin] -= _amount;
+        token_balances[_to] += _amount;
         return true;
     }
+
+    // transferPoint(requester, _amount);
 }

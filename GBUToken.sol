3d8a911f5674f70fe14e1d8415807e606a117b05
heemin1008 <125268228+heemin1008@users.noreply.github.com>
2023-06-14 16:48:17 +0900

    Update GBUToken.sol

--- GBUToken.sol
+++ GBUToken.sol
@@ -34,36 +34,14 @@ contract GBUToken is ERC20 {
         return balanceOf(admin);
     }
 
-    // 사용자의 토큰 잔액 조회
-    function Token_Balance_Of_User() public view returns (uint256) {
-        return token_balances[msg.sender];
-    }
 
-    // 토큰 전송하기
-    function transferToken(address to, uint256 amount) public returns (bool) {
-        require(amount > 0, "Invalid amount"); // amount가 0보다 커야함 
-
-        // 토큰 컨트랙트의 transfer 함수 호출
-        bool success = transfer(to, amount);
-
-        // Transfer 이벤트 발생
-        emit Transfer(msg.sender, to, amount);
-
-        // 관리자의 토큰 잔액과 사용자의 토큰 잔액 갱신 (해결) 
-        token_balances[admin] -= amount;
-        token_balances[to] += amount;
-
-        return success;  
-    }
-
-
-     //GBU Token 전송 기능
-      function TestTransfer(address _to, uint256 _amount) public virtual  returns (bool) {
+    // 토큰 전송 메소드 
+    function GBU_Token_Transfer(address _to, uint256 _amount) public virtual returns (bool) {
         _transfer(admin, _to, _amount);
         token_balances[admin] -= _amount;
         token_balances[_to] += _amount;
         return true;
     }
 
-    // transferPoint(requester, _amount);
+ 
 }
